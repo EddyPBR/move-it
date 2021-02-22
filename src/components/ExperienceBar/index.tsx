ba73763502e2cbc4a0e67e3eb0eb1f676e59d2bf
EddyPBR <edvaldojunior1310@gmail.com>
2021-02-22 14:26:24 -0300

    :octocat: Create React App | :package: Added experience bar component

---
+++ src/components/ExperienceBar/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }} />
+        <span style={{ left: '50%' }}></span>
+      </div>
+      <span>600 px</span>
+    </header>
+  );
+}
+
+export default ExperienceBar;
