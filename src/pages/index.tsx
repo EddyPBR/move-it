b8f07f260ff1574228575b58134c7b69e7fd0163
EddyPBR <edvaldojunior1310@gmail.com>
2021-02-23 10:58:21 -0300

    :package: Adding favicon |  Head tag into index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,17 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 
+import Head from "next/head";
+
 import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
