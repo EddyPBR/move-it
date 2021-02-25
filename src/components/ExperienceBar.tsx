4cef941482dcbe304cf1a8bf24c8ce2b5033d48e
EddyPBR <edvaldojunior1310@gmail.com>
2021-02-25 08:19:21 -0300

    :bug: Fixed ExperienceBar not grow with xp

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -15,8 +15,8 @@ export function ExperienceBar() {
     <header className={styles.experienceBar}>
       <span>0 xp</span>
       <div>
-        <div style={{ width: `${percentToNextLevel}` }} />
-        <span style={{ left: `${percentToNextLevel}` }}>
+        <div style={{ width: `${percentToNextLevel}%` }} />
+        <span style={{ left: `${percentToNextLevel}%` }}>
           {currentExperience}xp
         </span>
       </div>
