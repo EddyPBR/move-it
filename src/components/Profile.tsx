a0c6e0ca15b8e967a08f7e05d2b9d6f3ae4a7107
EddyPBR <edvaldojunior1310@gmail.com>
2021-03-03 14:47:34 -0300

    :package: Adding skeleton load on Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,20 +1,33 @@
+import { useSession } from "next-auth/client";
+
 import { useContext } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
 import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
   const { level } = useContext(ChallengesContext);
 
+  const [session, loading] = useSession();
+
   return (
-    <div className={styles.profileContainer}>
-      <img src="https://github.com/Eddypbr.png" alt="Edvaldo Junior" />
-      <div>
-        <strong>Edvaldo Junior</strong>
-        <p>
-          <img src="assets/icons/level.svg" alt="Level" />
-          Level {level}
-        </p>
+    <SkeletonTheme color="#dcdde0" highlightColor="#F0F0F0">
+      <div className={styles.profileContainer}>
+        {
+          loading
+          ? <Skeleton circle={true} height={80} width={80} duration={1.5} />
+          : <img src={session.user.image} alt={session.user.name} />
+        }
+        <div>
+          <strong>{loading ? <Skeleton width={100} duration={1.5} /> : session.user.name }</strong>
+          <p>
+            <img src="assets/icons/level.svg" alt="Level" />
+            Level {level}
+          </p>
+        </div>
       </div>
-    </div>
+    </SkeletonTheme>
   );
 }
