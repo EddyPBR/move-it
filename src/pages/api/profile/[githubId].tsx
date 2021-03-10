80bb0c07d9ab94c1e6ab3ae8c09cb80e52592ace
EddyPBR <edvaldojunior1310@gmail.com>
2021-03-10 14:26:13 -0300

    :package: Adding route to search a profile

---
+++ src/pages/api/profile/[githubId].tsx
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { mongooseConnection } from "../../../database/database";
+
+import ProfileModel, { IProfileSchema } from "../../../models/Profile";
+
+export default async (
+  request: NextApiRequest,
+  response: NextApiResponse
+): Promise<void> => {
+  const { method } = request;
+
+  try {
+    await mongooseConnection();
+  } catch (error) {
+    return response.status(500).json({
+      error: error?._message || "Fail to connect with database",
+    });
+  }
+
+  switch (method) {
+    case "GET": {
+      const githubId = request.query;
+
+      try {
+        const profile = await ProfileModel.findOne(githubId);
+
+        return profile
+          ? response.status(200).json(profile)
+          : response.status(404).json({ error: "Profile not found" });
+      } catch (error) {
+        return response.status(400).json({
+          error: error?._message || "Failed to search a user",
+        });
+      }
+    }
+
+    default: {
+      return response.status(404).json({
+        error: "Route not found",
+      });
+    }
+  }
+};
