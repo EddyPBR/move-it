ff514d1d7e0cca58cde6c4d0fd20b8b177bf1b8a
EddyPBR <edvaldojunior1310@gmail.com>
2021-03-12 09:20:46 -0300

    :package: Setting props to rank component

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -1,36 +1,45 @@
 import styles from "../styles/components/Rank.module.css";
 
-export function Rank() {
+interface IRankInterface {
+  position: number;
+  name: string;
+  imageUrl: string;
+  level: number;
+  challengesCompleted: number;
+  totalExperience: number;
+}
+
+export function Rank({position, name, imageUrl, level, challengesCompleted, totalExperience}: IRankInterface) {
   return (
     <div className={styles.rank}>
       <div className={styles.position}>
-        <b>1</b>
+        <b>{position}</b>
       </div>
 
       <div className={styles.info}>
         <div className={styles.userInfo}>
           <img
-            src="https://avatars.githubusercontent.com/u/48658479?s=400&u=51365e1c6a53cb7c4a0741bb7dda1ebcf64d4417&v=4"
-            alt="Edvaldo Junior"
+            src={imageUrl}
+            alt={name}
           />
           <div>
-            <b>Edvaldo Junior</b>
+            <b>{name}</b>
             <div>
               <img src="/assets/icons/level.svg" alt="level" />
-              <span>Level 43</span>
+              <span>Level {level}</span>
             </div>
           </div>
         </div>
 
         <div className={styles.otherInfo}>
           <span>
-            <b>127</b> completados
+            <b>{challengesCompleted}</b> completados
           </span>
         </div>
 
         <div className={styles.otherInfo}>
           <span>
-            <b>154000</b> xp
+            <b>{totalExperience}</b> xp
           </span>
         </div>
 
