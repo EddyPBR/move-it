d9affbe1879a662b0787ed5e27348e33d9d51504
EddyPBR <edvaldojunior1310@gmail.com>
2021-03-12 16:26:52 -0300

    :package: Adding Skeleton load on completed challenges value

--- src/components/CompletedChallenges.tsx
+++ src/components/CompletedChallenges.tsx
@@ -1,14 +1,29 @@
+import { useSession } from "next-auth/client";
+
 import { useContext } from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
 import { ChallengesContext } from "../contexts/ChallengesContext";
+
 import styles from "../styles/components/CompletedChallenges.module.css";
 
 export function CompletedChallenges() {
   const { challengesCompleted } = useContext(ChallengesContext);
 
+  const [session] = useSession();
+
   return (
-    <div className={styles.completedChallengesContainer}>
-      <span>Desafios completos</span>
-      <span>{String(challengesCompleted).padStart(2, "0")}</span>
-    </div>
+    <SkeletonTheme color="#dcdde0" highlightColor="#F0F0F0">
+      <div className={styles.completedChallengesContainer}>
+        <span>Desafios completos</span>
+        <span>
+        {
+          session 
+            ? (String(challengesCompleted).padStart(2, "0")) 
+            : (<Skeleton width={50} duration={1.5} />)
+        }
+        </span>
+      </div>
+    </SkeletonTheme>
   );
 }
